e805bd0fa0d1b85c62e9a9416fa0198c1731f3ef
Abdullah Gonul <abdullahgonol5@gmail.com>
2025-12-14 17:14:20 +0300

    14.12.2025- * EDA düzeltildi * Future Eng. bitirildi

--- epias_merge_dataset.py
+++ epias_merge_dataset.py
@@ -1,16 +1,30 @@
 from statistics import quantiles
-
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
 import glob
 import yfinance as yf
+from sklearn.exceptions import ConvergenceWarning
+import datetime
+import locale
 
 warnings.filterwarnings("ignore")
 pd.set_option('display.max_columns', None)
 
-#---------------------------
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=ConvergenceWarning)
+
+pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+
+######---------------------------
 # VERİ SETİ OKUMA
 #---------------------------
 
@@ -25,8 +39,8 @@ kgup_files = [
 ]
 kgup_dfs = []
 for file in kgup_files:
-    df = pd.read_csv(file,sep=";")
-    kgup_dfs.append(df)
+    df_final = pd.read_csv(file,sep=";")
+    kgup_dfs.append(df_final)
 kgup_df = pd.concat(kgup_dfs)
 kgup_df = kgup_df.drop_duplicates(subset=['Tarih', 'Saat']).reset_index(drop=True)
 
@@ -106,11 +120,11 @@ all_dates = pd.DataFrame({'Tarih': pd.date_range(start=start_date, end=end_date,
 all_dates['Tarih'] = all_dates['Tarih'].dt.normalize().dt.tz_localize(None)
 
 usd_data = pd.merge(all_dates, usd_data, on='Tarih', how='left')
-
 usd_data['Dolar_Kuru'] = usd_data['Dolar_Kuru'].ffill().bfill()
 
 # Ana veriye ekle
 df_final = pd.merge(df_final, usd_data, on='Tarih', how='left')
+
 
 
 
@@ -135,6 +149,12 @@ df_final.to_csv('EPIAS_.csv', index=False)
 df_final.head()
 
 
+
+
+
+
+
+##############################################################
 #---------------------------
 # EDA
 #---------------------------
@@ -153,7 +173,7 @@ def data_summary(dataframe, head=5):
 
 data_summary(df_final)
 
-def degisken_analiz(dataframe, cat_th=10, car_th=20):
+def degisken_analiz(dataframe, cat_th=2, car_th=20):
     # cat_cols, cat_but_car
     cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
     num_but_cat = [col for col in dataframe.columns if
@@ -186,8 +206,6 @@ def numeric_summary(dataframe, numerical_col, plot=False):
         plt.title(numerical_col)
         plt.show()
 
-
-
 print("\n--- NUMERİK DEĞİŞKENLERİN DAĞILIMI ---")
 for col in num_cols:
     numeric_summary(df_final, numerical_col=col, plot=True)
@@ -213,10 +231,218 @@ ax.set_title("Correlation Matrix", fontsize=20)
 plt.show(block = True)
 
 
+#---------------------------
+# Aykırı gözlem var mı incelemesi
+#---------------------------
+
+# Aykırı değerlerin baskılanması
+def outlier_thresholds(dataframe, variable, low_quantile=0.05, up_quantile=0.95):
+    quantile_one = dataframe[variable].quantile(low_quantile)
+    quantile_three = dataframe[variable].quantile(up_quantile)
+    interquantile_range = quantile_three - quantile_one
+    up_limit = quantile_three + 1.5 * interquantile_range
+    low_limit = quantile_one - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+# Aykırı değer kontrolü
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+# Aykırı değerlerin baskılanması
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+#  Aykırı değerlerin bulunduğu değişkenleri bulmak
+outlier_var = []
+for col in num_cols:
+    if check_outlier(df_final, col):
+        outlier_var.append(col)
+    if col != "SalePrice":
+        print(col, check_outlier(df_final, col))
+
+for var in outlier_var:
+    sns.boxplot(data=df_final, x=df_final[var])
+    plt.show()
+
+#---------------------------
+# FUTURE ENGİNEERİNG
+#---------------------------
+#  Outlier values
+for col in num_cols:
+    if col != "SalePrice":
+        replace_with_thresholds(df_final,col)
+
+# Kontrol edelim!!!
+for col in num_cols:
+    if col != "SalePrice":
+        print(col, check_outlier(df_final, col))
 
 
 
 
+df_final['TOPLAM_URETIM'] = (
+    df_final['Doğalgaz'] +
+    df_final['Linyit'] +
+    df_final['İthal Kömür'] +
+    df_final['Jeotermal'] +
+    df_final['Barajlı'] +
+    df_final['Akarsu'] +
+    df_final['Rüzgar'] +
+    df_final['Gunes'] +
+    df_final['Biyokütle']
+)
+
+df_final['YENILENEBILIR_TOPLAM'] = (
+    df_final['Rüzgar'] +
+    df_final['Gunes'] +
+    df_final['Barajlı'] +
+    df_final['Akarsu'] +
+    df_final['Jeotermal'] +
+    df_final['Biyokütle']
+)
+df_final['NET_YUK'] = df_final['Yük Tahmin Planı (MWh)'] - df_final['YENILENEBILIR_TOPLAM']
+
+
+df_final['TOPLAM_TERMIK_URETIM'] = (
+    df_final['Doğalgaz'] +
+    df_final['Linyit'] +
+    df_final['İthal Kömür']
+)
+
+df_final['YENILENEBILIR_ORANI'] = df_final['YENILENEBILIR_TOPLAM'] / df_final['TOPLAM_URETIM']
 
 
 
+
+df_final['PTF_lag_1'] = df_final['PTF (TL/MWh)'].shift(1)
+df_final['PTF_lag_24'] = df_final['PTF (TL/MWh)'].shift(24)
+df_final['PTF_lag_168'] = df_final['PTF (TL/MWh)'].shift(168)
+
+df_final['PTF_roll_mean_24'] = df_final['PTF (TL/MWh)'].rolling(24).mean()
+df_final['PTF_roll_std_24'] = df_final['PTF (TL/MWh)'].rolling(24).std()
+
+df_final.head(200)
+
+#Lag ve rolling sonrası ilk satırlar boş olur:
+
+####df_final = df_final.dropna().reset_index(drop=True)
+## Tüm verileri kontrol ettikten sonra silinecek !!!!
+
+
+# 1. DOĞALGAZ ETKİSİ (Gas Impact)
+# Mantık: O saatte ne kadar çok gaz yakılıyorsa, gazın birim fiyatı o kadar önem kazanır.
+# dogalgaz_fiyatlari_Mwh genelde TL bazlıdır (BOTAŞ Tarifesi). 
+# Eğer bu veri zaten TL ise Dolar ile çarpmaya gerek yoktur.
+
+df_final['Gas_Impact'] = df_final['Doğalgaz'] * df_final['dogalgaz_fiyatlari_Mwh']
+
+
+# 2. İTHAL KÖMÜR ETKİSİ (Coal Impact / FX Impact)
+# Mantık: İthal kömür yurt dışından Dolar ile alınır.
+# Elimizde kömürün ton fiyatı (API2 endeksi) yok ama Dolar Kuru var.
+# İthal Kömür üretimi arttıkça, Dolar kurunun maliyet üzerindeki baskısı artar.
+
+df_final['Coal_Impact'] = df_final['İthal Kömür'] * df_final['Dolar_Kuru']
+
+
+
+
+# 1. AYARLAR VE TATİL LİSTESİ
+# ---------------------------------------------------------
+# Tarih formatı ayarı (Senin kodundan alındı)
+try:
+    locale.setlocale(locale.LC_ALL, 'tr_TR.UTF-8')
+except locale.Error:
+    try:
+        locale.setlocale(locale.LC_ALL, 'Turkish_Turkey.1254')
+    except locale.Error:
+        pass
+
+
+def get_holiday_dates_2025():
+    """
+    Senin sağladığın listeden sadece tarih objelerini (datetime.date) 
+    bir liste olarak döndürür.
+    """
+    holidays = [
+        (datetime.date(2025, 1, 1), "Yılbaşı"),
+
+        # Ramazan Bayramı
+        (datetime.date(2025, 3, 29), "Ramazan Bayramı Arifesi"),
+        (datetime.date(2025, 3, 30), "Ramazan Bayramı 1. Gün"),
+        (datetime.date(2025, 3, 31), "Ramazan Bayramı 2. Gün"),
+        (datetime.date(2025, 4, 1), "Ramazan Bayramı 3. Gün"),
+
+        # Ulusal Bayramlar
+        (datetime.date(2025, 4, 23), "Ulusal Egemenlik ve Çocuk Bayramı"),
+        (datetime.date(2025, 5, 1), "Emek ve Dayanışma Günü"),
+        (datetime.date(2025, 5, 19), "Atatürk'ü Anma, Gençlik ve Spor Bayramı"),
+
+        # Kurban Bayramı
+        (datetime.date(2025, 6, 5), "Kurban Bayramı Arifesi"),
+        (datetime.date(2025, 6, 6), "Kurban Bayramı 1. Gün"),
+        (datetime.date(2025, 6, 7), "Kurban Bayramı 2. Gün"),
+        (datetime.date(2025, 6, 8), "Kurban Bayramı 3. Gün"),
+        (datetime.date(2025, 6, 9), "Kurban Bayramı 4. Gün"),
+
+        # Diğer Resmi Tatiller
+        (datetime.date(2025, 7, 15), "Demokrasi ve Milli Birlik Günü"),
+        (datetime.date(2025, 8, 30), "Zafer Bayramı"),
+        (datetime.date(2025, 10, 28), "Cumhuriyet Bayramı Arifesi"),
+        (datetime.date(2025, 10, 29), "Cumhuriyet Bayramı"),
+    ]
+
+    # Bize sadece tarihlerin olduğu bir liste lazım
+    return [date_obj for date_obj, name in holidays]
+
+
+# 2. VERİ SETİ HAZIRLIĞI (SİMÜLASYON)
+# ---------------------------------------------------------
+# Gerçek verin olmadığı için 2025 yılı için saatlik boş bir veri seti oluşturuyorum.
+# Sen kendi projende: df_final = pd.read_csv("verin.csv") yapacaksın.
+
+
+
+
+# 3. FEATURE ENGINEERING (ÖZELLİK MÜHENDİSLİĞİ)
+# ---------------------------------------------------------
+# Önce veriyi string yap, sonra ':' işaretinden böl, ilk parçasını al ve sayıya çevir.
+df_final['Saat'] = df_final['Saat'].astype(str).str.split(':').str[0].astype(int)
+
+# Şimdi orijinal kodunu tekrar çalıştırabilirsin
+df_final['Is_Peak_Hour'] = df_final['Saat'].apply(lambda x: 1 if 8 <= x <= 20 else 0)
+
+# Kontrol
+print(df_final[['Saat', 'Is_Peak_Hour']].head())
+# A) Tatil Listesini Alalım
+resmi_tatil_gunleri = get_holiday_dates_2025()
+
+# B) Is_Weekend (Hafta Sonu Mu?)
+# 5: Cumartesi, 6: Pazar
+df_final['Is_Weekend'] = pd.to_datetime(df_final['Tarih']).dt.dayofweek.isin([5, 6]).astype(int)
+
+# C) Is_Official_Holiday (Resmi Tatil Listesinde Var Mı?)
+# Tarih sütunu ile tatil listesini karşılaştırır
+df_final['Is_Official_Holiday'] = df_final['Tarih'].isin(resmi_tatil_gunleri).astype(int)
+
+# D) Is_Holiday (Genel Tatil Durumu)
+# Hafta sonu VEYA Resmi tatil ise 1 olsun. 
+# (Çünkü model için Pazar günü ile Bayram günü etkisi benzerdir: Talep düşer)
+df_final['Is_Holiday'] = (df_final['Is_Weekend'] | df_final['Is_Official_Holiday']).astype(int)
+
+# E) Is_Peak_Hour (Puant Saati Mi?)
+# Sabah 08:00 ile Akşam 20:00 (dahil) arası
+df_final['Is_Peak_Hour'] = df_final['Saat'].apply(lambda x: 1 if 8 <= x <= 20 else 0)
+
+# F) Is_Business_Day (İş Günü Mü?)
+# Tatil değilse iş günüdür. (Ters mantık)
+df_final['Is_Business_Day'] = 1 - df_final['Is_Holiday']
+
+df_final.head(200)
